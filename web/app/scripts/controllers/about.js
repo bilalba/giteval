22a17c48a569cb3c97d815fa2ba2c0e519388a7a
bilalba <tringingly@gmail.com>
2016-09-09 01:35:34 +0500

    added web component

---
+++ web/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gitevalApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the gitevalApp
+ */
+angular.module('gitevalApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ web/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gitevalApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gitevalApp
+ */
+angular.module('gitevalApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

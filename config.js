3781a6ff18fffd34ecbb67651e35d0beb5c9dab7
bilalba <tringingly@gmail.com>
2016-09-11 15:34:58 +0500

    h

--- config.js
+++
@@ -1,9 +0,0 @@
-var config = {};
-var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-config.port = 9910;
-
-config.github = {};
-config.github.CLIENT_ID = 'c45417c5d6249959a91d';
-config.github.CLIENT_SECRET = '3630a057d4ebbbdbfc84f855376f3f46f58b9710';
-
-module.exports = config;
